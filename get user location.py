b2780178b6523e10ebf19f2f12538e6d58a79d82
Aaro Tanner <tanner.aaro@gmail.com>
2026-02-25 15:01:11 +0200

    changed query to use %s

--- get user location.py
+++ get user location.py
@@ -4,16 +4,16 @@ conn = mysql.connector.connect(
     host='127.0.0.1',
     port=3306,
     database='cola_game',
-    user='',
-    password='',
+    user='root',
+    password='Aaro22',
     autocommit=True
 )
 
 def get_user_location(user_id):
     cursor = conn.cursor(dictionary=True)
 
-    sql = f"SELECT location FROM user_info where id = {user_id}"
-    cursor.execute(sql)
+    sql = f"SELECT location FROM user_info where id = %s"
+    cursor.execute(sql, (user_id,))
     result = cursor.fetchone()
 
     return result
