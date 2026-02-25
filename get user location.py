aaf8f6ca88c832df5e8121043ad82599c833510e
Aaro Tanner <tanner.aaro@gmail.com>
2026-02-25 13:20:02 +0200

    made a function to get users current location

---
+++ get user location.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3306,
+    database='cola_game',
+    user='',
+    password='',
+    autocommit=True
+)
+
+def get_user_location(id):
+    cursor = conn.cursor(dictionary=True)
+
+    sql = f"SELECT location FROM user_info where id = {id}"
+    cursor.execute(sql)
+    result = cursor.fetchone()
+
+    return result
