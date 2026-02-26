60d73a8e4956025e5a0337cbb977f0f22e98b5e1
Matti Pihlajaviita <matti.pihlajaviita@hotmail.com>
2026-02-26 12:39:57 +0200

    päivitetty calculate_distance

---
+++ calculate distance.py
@@ -0,0 +1,15 @@
+from geopy import distance
+from queryDb import queryDb
+
+def calculate_distance(currentLocation, targetCountry):
+
+    currentLocationXY = queryDb(f"SELECT longitude_deg, latitude_deg FROM airport WHERE ident = '{currentLocation}'")
+    targetCountryXY = queryDb(f"SELECT longitude_deg, latitude_deg FROM airport WHERE ident = '{targetCountry}'")
+
+    dist = distance.distance(currentLocationXY, targetCountryXY)
+
+    return dist
+
+
+
+
