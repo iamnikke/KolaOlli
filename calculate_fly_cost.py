9b30889d6d9d1d1216748f7c30f6c8f82d8e1c27
Nikke <nikkemarttila@gmail.com>
2026-02-26 15:21:21 +0200

    vaihe 1

--- calculate_fly_cost.py
+++ calculate_fly_cost.py
@@ -1,5 +1,4 @@
 def calculate_fly_cost(dist):
     price = dist * 0.2
-    print (f"Matkan hinta: {price:.2f} euroa")
     return price
 
--- move player.py
+++
@@ -1,7 +0,0 @@
-from queryDb import queryDb
-
-def move_player(icao, travel_distance, user_money, game_id):
-
-    queryDb(f"UPDATE user_info SET location = '{icao}', total_travel_km = '{travel_distance}', money = '{user_money}' WHERE id = '{game_id}'")
-
-    return True
