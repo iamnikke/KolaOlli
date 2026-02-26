2d2e18a75192b71a52f676bb23c7fa750bea0e9e
Matti Pihlajaviita <matti.pihlajaviita@hotmail.com>
2026-02-26 14:27:53 +0200

    päivitetty calculate_distance lisätty calculate_fly_cost

--- calculate_distance.py
+++ calculate_distance.py
@@ -3,13 +3,17 @@ from queryDb import queryDb
 
 def calculate_distance(currentLocation, targetCountry):
 
-    #
+    # haetaan lentokenttien koordinaatit tietokannasta
     currentLocationXY = queryDb(f"SELECT longitude_deg, latitude_deg FROM airport WHERE ident = '{currentLocation}'")
     targetCountryXY = queryDb(f"SELECT longitude_deg, latitude_deg FROM airport WHERE ident = '{targetCountry}'")
 
+    # laskee lentokenttien etäisyyden
     dist = distance.distance(currentLocationXY, targetCountryXY)
+    print (f"Etäisyys: {dist.km:.2f} km")
+    return dist.km
 
-    return dist
+dista = calculate_distance('agar','efhk')
+calculate_fly_cost(dista)
 
 
 
