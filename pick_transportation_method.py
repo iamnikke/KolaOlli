19df19c91e4dc2e7256f4903a2bb7965ae01657a
Aaro Tanner <tanner.aaro@gmail.com>
2026-03-02 13:36:24 +0200

    added function to select airplane

---
+++ pick_transportation_method.py
@@ -0,0 +1,17 @@
+class Airplane:
+    def __init__(self, name, speed, capacity,):
+        self.name = name
+        self.speed =speed
+        self.capacity = capacity
+
+    def __repr__(self):
+        return f"Lentokone: (Nimi: {self.name}, Nopeus: {self.speed} km/h, Kapasiteetti: {self.capacity} tölkkiä)"
+
+
+def init_vehicles():
+    small_airplane = Airplane("pieni lentokone", 1000, 50)
+    medium_airplane = Airplane("keskikokoinen lentokone", 1500, 500)
+    big_airplane = Airplane("iso lentokone", 2000, 2000)
+
+    # Palautetaan lista kulkuneuvo-olioista
+    return [small_airplane, medium_airplane, big_airplane]
