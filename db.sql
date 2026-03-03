576b4276515ae8efea46cc21bbb3aecd4ec205cc
Aaro Tanner <tanner.aaro@gmail.com>
2026-03-03 12:40:25 +0200

    moved all functions to a single file

--- db.sql
+++ db.sql
@@ -160,11 +160,14 @@ CREATE TABLE `user_info` (
   `bribes_succeeded` tinyint(1) DEFAULT 0,
   `caught` tinyint(1) DEFAULT 0,
   `sales` int(11) NOT NULL DEFAULT 0,
+  `home_port` varchar(40) DEFAULT 'EFHK',
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `username_2` (`username`),
+  UNIQUE KEY `username_3` (`username`),
   KEY `location` (`location`),
   CONSTRAINT `1` FOREIGN KEY (`location`) REFERENCES `airport` (`ident`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=794 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -173,7 +176,7 @@ CREATE TABLE `user_info` (
 
 LOCK TABLES `user_info` WRITE;
 /*!40000 ALTER TABLE `user_info` DISABLE KEYS */;
-INSERT INTO `user_info` VALUES (1,NULL,0.00,NULL,NULL,0.00,NULL,'EFHK',NULL,NULL,0,0,0);
+INSERT INTO `user_info` VALUES (1,NULL,0.00,NULL,NULL,0.00,NULL,'EFHK',NULL,NULL,0,0,0,'EFHK'),(303,'asdf',447.83,100,0,2760.86,0.00,'LFPG','16:00:00',0,0,0,0,'EFHK'),(318,'xx',447.83,100,0,2760.86,0.00,'lfpg','16:00:00',0,0,0,0,'EFHK'),(571,'moveplayertest',447.83,100,0,2760.86,0.00,'LFPG','16:00:00',0,0,0,0,'EFHK'),(791,'tess',447.83,100,0,2760.86,0.00,'lfpg','16:00:00',0,0,0,0,'EFHK'),(792,'kfk',2950.00,100,0,0.00,0.00,'efhk','16:00:00',0,0,0,0,'EFHK'),(793,'dd',2397.83,100,0,2760.86,0.00,'lfpg','16:00:00',0,0,0,0,'EFHK');
 /*!40000 ALTER TABLE `user_info` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -186,4 +189,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-02-26 13:12:10
+-- Dump completed on 2026-03-03 12:36:11
