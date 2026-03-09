e50f6cdcf4200c9fd6045c233a4a65d98534138a
Nikke <nikkemarttila@gmail.com>
2026-03-09 09:42:31 +0200

    UX & Terminal UI Improvements

---
+++ printSelectCountryHud.py
@@ -0,0 +1,30 @@
+from functions import *
+
+def printSelectCountryHud(playerData):
+
+    allowedCountries = {
+        'Austria': 'LOWW',
+        'Bryssel, Belgium': 'EBBR',
+        'Madrid, Espanja': 'LEMD',
+        'Rooma, Italia': 'LIRF',
+        'Luxemburg, Luxemburg': 'ELLX',
+        'Varsova, Puola': 'EPWA',
+        'Pariisi, Ranska': 'LFPG',
+        'Kööpenhamina, Tanska': 'EKCH',
+        'Ankara, Turkki': 'LTAC',
+        'Minsk, Valko-Venäjä': 'UMMS',
+        'Viro, Tallinna': 'EETN',
+    }
+
+    print("========================================")
+
+    for country, icao in allowedCountries.items():
+        dist = calculate_distance(playerData.location, icao)
+        price = float(f"{calculate_fly_cost(dist):.2f}")
+
+        print(icao + " --> " + country)
+        print("     ", int(dist), "km päässä | Meno-paluu", price, "€")
+        print("")
+
+
+    print("========================================")
